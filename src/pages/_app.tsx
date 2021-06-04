ce9361e779c56476f70b80cd284c6809b62ad115
CarolineCunha <caroline.cunha.b@hotmail.com>
2021-06-04 18:03:34 -0300

    componente header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,17 @@
 
 import { AppProps } from 'next/app';
-import '../styles/global.scss'
+import React from 'react';
+import '../styles/global.scss';
+import { Header } from '../components/Header';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Header />
+    <Component {...pageProps} />
+    </>
+  )
+ 
 }
 
 export default MyApp
