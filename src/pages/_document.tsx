040fcc1e93433905f1df7a69ebbc8c57ce7cbedd
CarolineCunha <caroline.cunha.b@hotmail.com>
2021-07-27 07:48:15 -0300

    chave

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,7 +12,7 @@ export default class MyDocument extends Document {
             <body>
                 <Main />
                 <NextScript />
-                <span></span>
+                
             </body>
         </Html>
         )
